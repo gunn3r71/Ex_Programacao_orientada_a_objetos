afdd4942459322bb98a2d138b02a045d22462b7a
Lucas <lucasgunner3@gmail.com>
2020-08-06 16:49:41 -0300

    Encapsulando os atributos

---
+++ video_42/Produto.cs
@@ -0,0 +1,93 @@
+﻿using System.Globalization;
+
+namespace video_42
+{
+    class Produto
+    {
+        //Padrão para atributos privados: Underline na frente e primeira letra minuscula
+        private string _nome;
+        public double Preco { get; private set; }    //Criando uma Auto-Property, é usada quando a property
+        public int Quantidade { get; private set; } //não tem lógica especifíca.. está dizendo que é permitida
+                                                   //somente a visualização.
+
+        public Produto(string nome, double preco, int quantidade) //Crindo um CONSTRUTOR que pede assim que é 
+        {                                                        //Instanciado que os atributos sejam
+            _nome = nome;                                       //Inseridos.
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public Produto(string nome, double preco) //Criando uma SOBRECARGA de construtor que solicita 
+        {                                        //somente alguns parametros
+            _nome = nome;                       //Construtor Opcional
+            Preco = preco;
+            Quantidade = 0;
+        }
+
+
+
+        // Criando as properties para o encapsulamento dos atributos da classe //
+        public string Nome{
+            get
+            {
+                return _nome;
+            }
+            set
+            {
+                if (value!=null && value.Length > 1)
+                {
+                    _nome = Nome;
+                }
+            }
+        }
+        /* As auto-Properties substituem este código 
+         * 
+        public double Preco
+        {
+            get
+            {
+                return _preco;
+            }
+        }
+
+        public int Quantidade
+        {
+            get
+            {
+               return _quantidade;
+            }
+        }
+        */
+        //--------------------------------------------------------------------------------//
+
+
+        public double ValorTotalEmEstoque()
+        {
+            return Quantidade * Preco;
+        }
+
+
+        //somente estes dois métodos poderam modificar quantidade//
+        public void AdicionarProduto(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProduto(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        //--------------------------------------------------------------------------------//
+
+        public override string ToString() //o override SOBREESCREVE um método de uma outra classe!
+        {
+            return Nome
+                + ", $"
+                + Preco.ToString("F2",CultureInfo.InvariantCulture)
+                + ", Quantidade: " 
+                + Quantidade
+                + ", Valor Total no estoque: "
+                + ValorTotalEmEstoque().ToString("F2",CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ video_42/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace video_42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Produto p = new Produto(); //esta váriavel costuma ser chamada váriavel composta
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("Quantidade em estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Produto p = new Produto(nome, preco, quantidade);//Instanciando o produto com construtor personalizado
+
+            Console.WriteLine("Dados do produto: " + p); //o c# já converte implicitamento o objeto p para string
+            Console.WriteLine("Digite a quantidade de produtos a ser inserido: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProduto(qte);
+            Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine("Digite a quantidade de produtos a ser removido: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProduto(qte);
+            Console.WriteLine("Dados atualizados: " + p);
+        }
+    }
+}
